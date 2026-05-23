5bff67e01c1e791ccb343c6cfc7460b484152617
Suami Yonashiro <suami.yy@gmail.com>
2026-05-23 12:51:50 -0300

    adicionado exercício 11

--- python-avancado-para-dados/aula-02/exercicios.py
+++ python-avancado-para-dados/aula-02/exercicios.py
@@ -63,6 +63,10 @@ import math
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+#palavra = input("Escreva uma palavra para converter para letras maiúsculas: ")
+#resultado = palavra.upper()
+#print(f"{resultado}.")
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
