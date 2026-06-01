b714c3111c48660458f5c5d343f58deda8b6ba92
Suami Yonashiro <suami.yy@gmail.com>
2026-06-01 14:04:56 -0300

    Merge com o repositorio remoto e atualizacao de pastas

---
+++ 03-python-avancado-para-dados/aula-01/exemplo_01.py
@@ -0,0 +1,6 @@
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres.
+
+nome = input("Digite o seu nome: ")
+tamanho = len(nome)
+
+print(f"Seu nome tem: {tamanho} caracteres")
---
+++ 03-python-avancado-para-dados/aula-01/exemplo_02.py
@@ -0,0 +1,6 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma.
+
+num_01 = float(input("Digite o primeiro valor: "))
+num_02 = float(input("Digite o segundo valor: "))
+
+print(f"A soma dos dois valores é igual a: {num_01 + num_02}")
