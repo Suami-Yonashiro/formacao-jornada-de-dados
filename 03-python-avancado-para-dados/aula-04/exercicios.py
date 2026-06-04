1b506732b9bee43d0fc31e9a4d1c0244c0947191
Suami Yonashiro <suami.yy@gmail.com>
2026-06-04 09:31:39 -0300

    feat: Adicionado exercício 3

--- 03-python-avancado-para-dados/aula-04/exercicios.py
+++ 03-python-avancado-para-dados/aula-04/exercicios.py
@@ -2,3 +2,15 @@
 
 # 1. Lista de números ao quadrado. 
 # Reposta: 
+
+# 2.
+
+# 3. Informações de um livro. Crie um dicionário para armazenar informações de um livro. 
+from typing import Dict, Any
+livro: dict[str, Any] = {
+    "Titulo": "Game of Thrones A Guerra dos Tronos",
+    "Autor": "George R. R. Martin",
+    "Ano": 1996, 
+}
+for chave, valor in livro.items():
+    print(f"{chave}: {valor}") 
