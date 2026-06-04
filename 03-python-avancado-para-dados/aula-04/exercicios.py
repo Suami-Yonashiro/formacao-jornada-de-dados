eecda2971354f395d8abddb9be94f3f495dd283a
Suami Yonashiro <suami.yy@gmail.com>
2026-06-04 18:37:15 -0300

    feat: Exercício 2

--- 03-python-avancado-para-dados/aula-04/exercicios.py
+++ 03-python-avancado-para-dados/aula-04/exercicios.py
@@ -1,13 +1,18 @@
 ### Exercícios de Listas e Dicionários resolvidos.
 
-# 1. Lista de números ao quadrado. 
+# 1. Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
 # Reposta:
-numeros: list[int] = list(range(0, 11))
-for numero in numeros:
-    quadrado = numero**2
-    print(f"O quadrado de:\n{numero} = {quadrado}")
+# numeros: list[int] = list(range(0, 11))
+# for numero in numeros:
+#     quadrado = numero**2
+#     print(f"O quadrado de:\n{numero} = {quadrado}")
 
-# 2.
+# 2. Dada a lista `["Python", "Java", "C++", "JavaScript"]`, remova o item "C++" e adicione "Ruby".
+# Resposta: 
+# linguagens: list[str] = ["Python", "Java", "C++", "JavaScript"]
+# linguagens.remove("C++")
+# linguagens.append("Ruby")
+# print(linguagens)
 
 # 3. Informações de um livro. Crie um dicionário para armazenar informações de um livro. 
 # from typing import Dict, Any
