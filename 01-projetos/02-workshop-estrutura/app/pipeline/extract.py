89a9786fa2cee3d5275cd4ab39c048171bf2ee72
Suami Yonashiro <suami.yy@gmail.com>
2026-06-13 13:04:19 -0300

    feat: Continuando projeto

--- 01-projetos/02-workshop-estrutura/app/pipeline/extract.py
+++ 01-projetos/02-workshop-estrutura/app/pipeline/extract.py
@@ -14,18 +14,17 @@ args: input_path (str): caminho da pasta com os arquivos
 return: lista de dataframes 
 """
 
-path = "data/input"
-
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
     # lista de arquivos
     all_files = glob.glob(os.path.join(path, "*.xlsx"))
     
     data_frame_list = []
     for file in all_files:
-        data_frame_list.append(pd.read_excel(file))
+        data = pd.read_excel(file)
+        data_frame_list.append(data)
         
     return data_frame_list
 
 if __name__ == "__main__":
-    data_frame_list = extract_from_excel(path)
+    data_frame_list = extract_from_excel("data/input")
     print(data_frame_list)
