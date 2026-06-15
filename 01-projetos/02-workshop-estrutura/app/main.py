f47fa8d813aca89b42406ff759b106a041508dd2
Suami Yonashiro <suami.yy@gmail.com>
2026-06-15 10:20:05 -0300

    feat:transform and load

--- 01-projetos/02-workshop-estrutura/app/main.py
+++ 01-projetos/02-workshop-estrutura/app/main.py
@@ -1,4 +1,10 @@
 from pipeline.extract import extract_from_excel
+from pipeline.transform import contact_data_frames
+from pipeline.load import load_excel
 
-lista_de_data_frame = extract_from_excel("data/input")
-print(lista_de_data_frame)
+if __name__ == "__main__":
+    data_frame_list = extract_from_excel("data/input")
+    print(type(data_frame_list))
+    data_frame = contact_data_frames(data_frame_list)
+    print(type(data_frame))
+    load_excel(data_frame, "data/output", "output")
