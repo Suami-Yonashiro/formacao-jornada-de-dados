c19973bbae4a7f5fe9385dede89ecfc667910ea7
Suami Yonashiro <suami.yy@gmail.com>
2026-06-23 11:04:26 -0300

    feat: Atualiza arquivo using_polars.py

--- 03-python-avancado-para-dados/aula-05/src/using_polars.py
+++ 03-python-avancado-para-dados/aula-05/src/using_polars.py
@@ -1,29 +1,57 @@
 import polars as pl
+import time
+from pathlib import Path
 
-# Created by Koen Vossen, 
+FILENAME = Path("data/measurements.txt")
+
+# Polars, created by Koen Vossen, 
 # Github: https://github.com/koenvo
 # Twitter/x Handle: https://twitter.com/mr_le_fox
 # https://x.com/mr_le_fox/status/1741893400947839362?s=20
-def create_polars_df():
-    pl.Config.set_streaming_chunk_size(4000000)
+
+def criar_dataframe_polars(path_do_txt: Path):
+    """Processar arquivo usando o motor de streaming otimizado do Polars."""
+    # Define o tamanho do bloco para esteira de streaming 
+    pl.Config.set_streaming_chunk_size(1000000)
+    
+    # pl.scan_csv inicia o modo "lazy". Ele apenas scaneia a estrutura do arquivo.
     return (
-        
-        pl.scan_csv("data/measurements.txt", separator=";", has_header=False, new_columns=["station", "measure"], schema={"station": pl.String, "measure": pl.Float64})
-        .group_by(by="station")
+        pl.scan_csv(
+            path_do_txt,
+            separator=";",
+            has_header=False,
+            new_columns=["station", "measure"],
+            # Lemos tudo inicialmente como string para não quebrar a leitura
+            schema={"station": pl.String, "measure": pl.String},
+            encoding="utf8-lossy",
+        )
+        # Tratamento explícito da coluna: Substitui ',' por '.' se necessário e converte para Float64
+        .with_columns(
+            pl.col("measure").str.replace(",", ".").cast(pl.Float64)
+        ) 
+        .group_by("station")
         .agg(
             max = pl.col("measure").max(),
             min = pl.col("measure").min(),
             mean = pl.col("measure").mean()
         )
         .sort("station")
-        .collect(streaming=True)
+        # O .collect(streaming=True) é o gatilho que liga os motores em Rust para processar
+        .collect(engine="streaming")
     )
 
 if __name__ == "__main__":
-    import time
-
+    # 1M 0.11 segundos
+    print("Iniciando a execução do script com Polars...") 
     start_time = time.time()
-    df = create_polars_df()
+    
+    if not FILENAME.exists():
+        print("Erro: O arquivo {FILENAME} não foi localizado.")
+        exit()
+        
+    df = criar_dataframe_polars(FILENAME)
     took = time.time() - start_time
-    print(df)
-    print(f"Polars Took: {took:.2f} sec")
+    
+    print("\n--- Resultados Finais (Polars) ---")
+    print(df.head())
+    print(f"\nPolars concluído em: {took:.2f} segundos.")
