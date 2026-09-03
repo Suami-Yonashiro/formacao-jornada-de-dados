8d669f1327ee6c6764113ee0babefdde373a6324
Suami Yonashiro <suami.yy@gmail.com>
2026-09-03 17:26:21 -0300

    feat: adiciona a resposta do exercício Q3.

--- 03-python-avancado-para-dados/aula-03/3.3-Q3.py
+++ 03-python-avancado-para-dados/aula-03/3.3-Q3.py
@@ -16,3 +16,25 @@ Depois pergunte ao usuário o nome de uma seleção e informe se ela está class
 
 Desafio: explique por que, mesmo digitando uma seleção repetida, ela aparece apenas uma vez no conjunto.
 """
+
+classificados = set()
+
+for c in range(1, 9):
+    selecao = input(f"Qual a {c}° seleção classificada? ")
+    classificados.add(selecao)
+
+print("-" * 40)
+print("SELEÇÕES CLASSIFICADAS NA FASE DE GRUPOS")
+print("-" * 40)
+
+print(f"As seleções classificadas foram: {', '.join(classificados)}.")
+
+print(f"\nO total de seleções diferentes cadastradas: {len(classificados)}.")
+print("-" * 40)
+
+pergunta = input("Digite o nome da seleção para verificar se ela está classificada: ")
+
+if pergunta in classificados:
+    print(f"Sim, a seleção do(a) {pergunta} está classificada.")
+else:
+    print(f"Não, a seleção do(a) {pergunta} não está entre os classificados.")
